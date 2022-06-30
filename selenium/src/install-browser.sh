c3c50f45df00c3eb42910f670173e80daed6c2bd
Rehan Haider <email@rehanhaider.com>
2022-07-01 01:34:48 +0530

    Removed version output

--- selenium/src/install-browser.sh
+++ selenium/src/install-browser.sh
@@ -7,9 +7,6 @@ curl "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshot
 unzip /tmp/chromium.zip -d /tmp/
 mv /tmp/chrome-linux/ /opt/chrome
 
-echo "Chromium version"
-/opt/chrome/chrome --version
-
 curl "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$CHROMIUM_VERSION%2Fchromedriver_linux64.zip?generation=1652397753719852&alt=media" > /tmp/chromedriver_linux64.zip
 unzip /tmp/chromedriver_linux64.zip -d /tmp/
 mv /tmp/chromedriver_linux64/chromedriver /opt/chromedriver
