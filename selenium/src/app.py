9d3aa4b4fe7bdada94e75f04ee8e0004e23bd1c4
Rehan Haider <email@rehanhaider.com>
2022-06-30 18:56:11 +0000

    FEAT: Version Selection

--- selenium/src/app.py
+++ selenium/src/app.py
@@ -2,6 +2,7 @@
 import time
 import json
 import os.path
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -11,19 +12,20 @@ from selenium.webdriver.chrome.options import Options
 
 def handler(event=None, context=None):
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "/opt/chrome/stable/chrome"
+    chrome_options.binary_location = "/opt/chrome/chrome"
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-dev-tools")
     chrome_options.add_argument("--no-zygote")
-    chrome_options.add_argument("--single-process")
     chrome_options.add_argument("window-size=2560x1440")
     chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+    chrome_options.add_argument("--data-path=/tmp/chrome-user-data")
+    chrome_options.add_argument("--disk-cache-dir=/tmp/chrome-user-data")
     chrome_options.add_argument("--remote-debugging-port=9222")
-    chrome_options.add_argument("--user-data-dir=/tmp/chrome-user-data")
-    chrome = webdriver.Chrome("/opt/chromedriver/stable/chromedriver", options=chrome_options)
+    chrome = webdriver.Chrome("/opt/chromedriver", options=chrome_options)
     chrome.get("https://cloudbytes.dev/")
     description = chrome.find_element(By.NAME, "description").get_attribute("content")
     print(description)
--- selenium/src/install-browser.sh
+++ selenium/src/install-browser.sh
@@ -1,36 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-declare -A chrome_versions
 
-# Enter the list of browsers to be downloaded
-### Using Chromium as documented here - https://www.chromium.org/getting-involved/download-chromium
-chrome_versions=( ['89.0.4389.47']='843831' )
-chrome_drivers=( "89.0.4389.23" )
-#firefox_versions=( "86.0" "87.0b3" )
-#gecko_drivers=( "0.29.0" )
+echo "Downloading Chromium..."
+curl "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$CHROMIUM_VERSION%2Fchrome-linux.zip?generation=1652397748160413&alt=media" > /tmp/chromium.zip
 
-# Download Chrome
-for br in "${!chrome_versions[@]}"
-do
-    echo "Downloading Chrome version $br"
-    mkdir -p "/opt/chrome/stable"
-    curl -Lo "/opt/chrome/stable/chrome-linux.zip" \
-    "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${chrome_versions[$br]}%2Fchrome-linux.zip?alt=media"
-    unzip -q "/opt/chrome/stable/chrome-linux.zip" -d "/opt/chrome/stable/"
-    mv /opt/chrome/stable/chrome-linux/* /opt/chrome/stable/
-    rm -rf /opt/chrome/stable/chrome-linux "/opt/chrome/stable/chrome-linux.zip"
-done
+unzip /tmp/chromium.zip -d /tmp/
+mv /tmp/chrome-linux/ /opt/chrome
 
-# Download Chromedriver
-for dr in ${chrome_drivers[@]}
-do
-    echo "Downloading Chromedriver version $dr"
-    mkdir -p "/opt/chromedriver/stable/"
-    curl -Lo "/opt/chromedriver/stable//chromedriver_linux64.zip" \
-    "https://chromedriver.storage.googleapis.com/$dr/chromedriver_linux64.zip"
-    unzip -q "/opt/chromedriver/stable//chromedriver_linux64.zip" -d "/opt/chromedriver/stable/"
-    chmod +x "/opt/chromedriver/stable/chromedriver"
-    rm -rf "/opt/chromedriver/stable/chromedriver_linux64.zip"
-done
+echo "Chromium version"
+/opt/chrome/chrome --version
 
-echo "Chrome & Chromedriver installed"
+curl "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$CHROMIUM_VERSION%2Fchromedriver_linux64.zip?generation=1652397753719852&alt=media" > /tmp/chromedriver_linux64.zip
+unzip /tmp/chromedriver_linux64.zip -d /tmp/
+mv /tmp/chromedriver_linux64/chromedriver /opt/chromedriver
